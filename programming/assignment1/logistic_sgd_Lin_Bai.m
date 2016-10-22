500199ebd6449237eee827657953c1b41546726e
Lin Bai <lbai.ee.ethz@gmail.com>
2016-10-22 17:15:38 +0200

    first programming assignment logistic sgd draft

---
+++ programming/assignment1/logistic_sgd_Lin_Bai.m
@@ -0,0 +1,30 @@
+function [ theta ] = logistic_sgd_Lin_Bai( data, labels )
+
+	N = size(data,2); % number of training samples
+	M = size(data,1); % values in each sample
+	theta = zeros(M,1);
+	theta_new = zeros(M,1);
+	diff_J = zeros(M,1);
+
+	alpha = 0.25;
+
+% the function g(theta_T*x)=1/(1+theta_T*x)
+% the hypothesis function is
+% h_theta(x)=1/(1+theta_T*x)
+
+% update theta = theta - alpha*diff(J(theta))
+
+	for k = 1:N
+%		for j = 1:M
+%			diff_J = (1/(1+transpose(theta)*data(:,k))-labels(j))*data(j,k);
+%			theta_new(j)=theta(j)-alpha*diff_J;
+%		end
+		diff_J = (1/(1+transpose(theta)*data(:,k))-labels)*data(:,k);
+		theta_new=theta-alpha*diff_J;
+		theta = theta_new;
+	end
+
+% theta = [ randn( size(data,2), 1 ); 0 ];
+
+end
+
