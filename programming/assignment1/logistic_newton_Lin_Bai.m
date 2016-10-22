9bf712baaf317501a927ce691cfc81e584d626c4
Lin Bai <lbai.ee.ethz@gmail.com>
2016-10-22 21:38:43 +0200

    correct sgd, add newton

---
+++ programming/assignment1/logistic_newton_Lin_Bai.m
@@ -0,0 +1,28 @@
+function [ theta ] = logistic_newton_Lin_Bai( data, labels )
+	
+	N = size(data,2); % number of training samples
+	M = size(data,1); % number of value in each sample
+	theta = zeros(M,1);
+	
+	iterationNo = 10;
+	J = zeros(iterationNo,1);
+	
+	for k = 1:iterationNo
+		% hypothesis function
+		% h = 1/(1+exp(g))
+		% g = theta*data
+		h = 1.0./(1.0+exp(transpose(theta)*data));
+
+		% gradient descent
+		gd = (1/N).*(h-labels)*data';
+		hessian = (1/N).*data*diag(h)*diag(1-h)*data';
+	
+		% cost function
+		J(k) = (1/N)*sum(-labels.*log(h)-(1-labels).*log(1-h));
+
+		theta = theta-hessian\gd';
+	end
+
+% theta = [ randn( size(data,2), 1 ); 0 ];
+
+end
--- programming/assignment1/logistic_sgd_Lin_Bai.m
+++ programming/assignment1/logistic_sgd_Lin_Bai.m
@@ -1,14 +1,15 @@
 function [ theta ] = logistic_sgd_Lin_Bai( data, labels )
-
+%	data=xtrain;
+%	labels=single(ytrain > 0);
 	N = size(data,2); % number of training samples
-	M = size(data,1); % values in each sample
+	M = size(data,1); % number of value in each sample
 	theta = zeros(M,1);
 	theta_new = zeros(M,1);
 	diff_J = zeros(M,1);
 
-	alpha = 0.25;
+	alpha = 0.05;
 
-% the function g(theta_T*x)=1/(1+theta_T*x)
+% the function g(theta_T*x)=1/(1+exp(theta_T*x))
 % the hypothesis function is
 % h_theta(x)=1/(1+theta_T*x)
 
@@ -16,10 +17,10 @@ function [ theta ] = logistic_sgd_Lin_Bai( data, labels )
 
 	for k = 1:N
 %		for j = 1:M
-%			diff_J = (1/(1+transpose(theta)*data(:,k))-labels(j))*data(j,k);
+%			diff_J = (1/(1+exp(transpose(theta)*data(:,k)))-labels(k)*data(j,k);
 %			theta_new(j)=theta(j)-alpha*diff_J;
 %		end
-		diff_J = (1/(1+transpose(theta)*data(:,k))-labels)*data(:,k);
+		diff_J = (1/(1+exp(transpose(theta)*data(:,k)))-labels(k)).*data(:,k);
 		theta_new=theta-alpha*diff_J;
 		theta = theta_new;
 	end
